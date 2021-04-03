f4a6416f6b853972290a5ec1d2a5fd3a55db992b
Awaliyatul Hikmah <57889114+ahikmah@users.noreply.github.com>
2021-04-03 09:52:43 +0700

    fixed query on my course list for instructors

--- src/models/instructor.js
+++ src/models/instructor.js
@@ -28,12 +28,13 @@ const getSpesificMemberProgress = (idCourse, idUser) => {
 };
 
 const getMyCourse = (idUser) => {
-    const qs = `SELECT c.name, c.start_time, c.end_time, COUNT(sc.student_id) AS member FROM courses c JOIN student_course sc ON c.id = sc.course_id WHERE c.instructor_id= ?  GROUP BY c.name`;
+    const qs = `SELECT c.name, ct.name, c.description, DATE_FORMAT(c.schedule, '%W') as day, c.start_time, c.end_time, COUNT(sc.student_id) AS member  FROM courses c JOIN student_course sc ON c.id = sc.course_id  JOIN categories ct ON c.category_id = ct.id WHERE c.instructor_id=? GROUP BY c.name UNION SELECT c.name, ct.name, c.description, DATE_FORMAT(c.schedule, '%W') as day, c.start_time, c.end_time, 0 as member FROM courses c JOIN categories ct ON c.category_id = ct.id WHERE c.instructor_id=? && c.name NOT IN (SELECT c.name FROM courses c JOIN student_course sc ON c.id = sc.course_id)`;
     return new Promise((resolve, reject) => {
-        dbMysql.query(qs, idUser, (err, result) => {
+        dbMysql.query(qs, [idUser, idUser], (err, result) => {
             if (err) {
                 reject(err);
             } else {
+                console.log(result);
                 resolve(result);
             }
         });
