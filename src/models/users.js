1580dda97f6271084b052a859f03c9d00ee5ed5d
Awaliyatul Hikmah <57889114+ahikmah@users.noreply.github.com>
2021-04-08 02:47:09 +0700

    update

--- src/models/users.js
+++ src/models/users.js
@@ -49,45 +49,41 @@ const loginUser = (data) => {
         dbMysql.query(qs, loginData, (err, result) => {
             if (err) return reject(err);
 
-            if (result.length > 0) {
-                bcrypt.compare(
-                    data.password,
-                    result[0].password,
-                    (err, isValid) => {
-                        if (err) return reject(err);
-                        if (!isValid) {
-                            result = false;
-                            return resolve(result);
-                        }
-
-                        const payload = {
-                            username: result[0].username,
-                            role:
-                                result[0].role === 1
-                                    ? 'facilitator'
-                                    : 'student',
-                        };
-
-                        const options = {
-                            expiresIn: process.env.EXPIRE,
-                            issuer: process.env.ISSUER,
-                        };
-
-                        jwt.sign(
-                            payload,
-                            process.env.SECRET_KEY,
-                            options,
-                            (err, token) => {
-                                if (err) return reject(err);
-                                resolve(token);
-                            }
-                        );
-                    }
-                );
-            } else {
+            if (result.length === 0) {
                 result = false;
-                resolve(result);
+                return resolve(result);
             }
+            bcrypt.compare(
+                data.password,
+                result[0].password,
+                (err, isValid) => {
+                    if (err) return reject(err);
+                    if (!isValid) {
+                        result = false;
+                        return resolve(result);
+                    }
+
+                    const payload = {
+                        username: result[0].username,
+                        role: result[0].role === 1 ? 'facilitator' : 'student',
+                    };
+
+                    const options = {
+                        expiresIn: process.env.EXPIRE,
+                        issuer: process.env.ISSUER,
+                    };
+
+                    jwt.sign(
+                        payload,
+                        process.env.SECRET_KEY,
+                        options,
+                        (err, token) => {
+                            if (err) return reject(err);
+                            resolve(token);
+                        }
+                    );
+                }
+            );
         });
     });
 };
